b950bb31c81b6f7075dca87496f541f3972d9e3c
fullNelson14 <bradjnelson14@gmail.com>
2019-03-02 11:59:16 -0700

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,42 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: `#f44336`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0.45rem 1.0rem`,
+      }}
+    >
+      <h1 style={{ margin: 0, fontSize: 14 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Looking for a website?</h1>
+    <p>Thanks for stopping by our new Gatsby site.</p>
+    <p>Now go build something great, and dream big.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/brad/">BRAD</Link>
+  </Layout>
+)
+
+export default IndexPage
